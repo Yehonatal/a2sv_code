6bd559492a6022a703d1d42c3f650f379e5ce925
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-23 23:13:25 +0300

    Update: leetcode python boiler plus note

---
+++ leetcode/dsa4.py
@@ -0,0 +1,10 @@
+def maxSum(grid):
+    m = len(grid)
+    n = len(grid[0])
+    ...
+
+
+grid = [[6, 2, 1, 3], [4, 2, 1, 5], [9, 2, 8, 7], [4, 1, 2, 9]]
+print(max(grid))
+grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(max(grid))
